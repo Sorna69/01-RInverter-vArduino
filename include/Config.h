9f3ddc3e79b51771060e7352da6ee149b34d7da8
Xan <xanoterovizan@gmail.com>
2022-08-22 08:05:06 +0200

    nose que hice

--- include/Config.h
+++ include/Config.h
@@ -3,21 +3,21 @@
 #define PWM_B 18
 #define DIS 22
 
-
 // Configuración modo AP
 const char *ssid_AP = "ESP32-RI";
 const char *password_AP = "casa2030";
+const char* hostname = "ESP32-RInv";
+
 
 IPAddress ip_AP(192, 168, 1, 222);
 IPAddress gateway_AP(192, 168, 1, 1);
 IPAddress subnet_AP(255, 255, 255, 0);
 
 // Configuración moodo STA
-//const char *ssid_STA = "NETGEAR56";
-//const char *password_STA = "casa2020";
-const char *ssid_STA = "1B";
-const char *password_STA = "Caseriumadorro_N36";
-const char* hostname = "ESP32-RInv";
+const char *ssid_STA = "NETGEAR56";
+const char *password_STA = "casa2020";
+//const char *ssid_STA = "1B";
+//const char *password_STA = "Caseriumadorro_N36";
 
 IPAddress ip_STA(192, 168, 0, 222);
 IPAddress gateway_STA(192, 168, 0, 1);
--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@ void setup()
   Serial.begin(115200);
   SPIFFS.begin();
 
-  //Conectar_WiFi_AP();
+  // Conectar_WiFi_AP();
   Conectar_WiFi_STA();
   InitServer();
   InitWebSockets();
@@ -34,11 +34,29 @@ void setup()
   digitalWrite(PWM_B, LOW);
 
   enablePCOUNTERCLK();
-  confGPIOPCNT();
+
+  Serial.print("Registro PCOUNTer antes: ");
+  Serial.println(  REG_GET_FIELD(GPIO_FUNC39_IN_SEL_CFG_REG,GPIO_FUNC39_IN_SEL));
+   confGPIOPCNT();
+  Serial.print("Registro PCOUNTer despues: ");
+  Serial.println(REG_GET_FIELD(GPIO_FUNC39_IN_SEL_CFG_REG,GPIO_FUNC39_IN_SEL));
+
+  //Serial.print("Registro PCOUNTer antes: ");
+  //Serial.println(REG_READ(PCNT_U0_CONF0_REG));
   confPCOUNTER();
+  //Serial.print("Registro PCOUNTer despues: ");
+  //Serial.println(REG_READ(PCNT_U0_CONF0_REG));
+
+  //Serial.print("Registro PCOUNTer antes: ");
+  //Serial.println(REG_READ(PCNT_CTRL_REG));
+  startPCOUNTER();
+  //Serial.print("Registro PCOUNTer despues: ");
+  //Serial.println(REG_READ(PCNT_CTRL_REG));
 }
 
 void loop()
 {
-
+  Serial.print("Counter value: ");
+  Serial.println(REG_READ(PCNT_U0_CNT_REG));
+  delay(1000);
 }
