7b2a9c137cdbb0913c18e86dd92f2af84ee3b09d
Sorna69 <elrossan@gmail.com>
2025-02-05 16:24:11 +0100

    Actualizado nombre de Red e IP

--- include/Config.h
+++ include/Config.h
@@ -2,10 +2,10 @@
 #define DIS 22
 
 // Configuración modo AP
-const char *ssid_AP = "ESP32-RI";
+const char *ssid_AP = "ESP32-AP";
 const char *password_AP = "casa2030";
 
-IPAddress ip_AP(192, 168, 1, 222);
+IPAddress ip_AP(192, 168, 1, 10);
 IPAddress gateway_AP(192, 168, 1, 1);
 IPAddress subnet_AP(255, 255, 255, 0);
 
@@ -14,6 +14,6 @@ const char *ssid_STA = "NETGEAR56";
 const char *password_STA = "casa2020";
 const char* hostname = "ESP32-RInv";
 
-IPAddress ip_STA(192, 168, 0, 222);
+IPAddress ip_STA(192, 168, 0, 10);
 IPAddress gateway_STA(192, 168, 0, 1);
 IPAddress subnet_STA(255, 255, 255, 0);
